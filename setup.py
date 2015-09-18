ecc3bc172d5ae83ef484799a33652d5b1a221d9f
Schon Peter <slapec@codehouse.hu>
2015-09-18 11:08:45 +0200

    Damn you migrations

--- setup.py
+++ setup.py
@@ -9,13 +9,14 @@ vcs_dependencies = [
 
 setup(
     name='django-yaat',
-    version='1.2.0',
+    version='1.3.0',
     packages=['yaat'],
     install_requires=['Django>=1.8.4', 'django-restify-framework==0.23', 'django-ordered-model'],
+    include_package_data=True,
     dependency_links=vcs_dependencies,
     description='The django-restify-framework resource of the yaat AngularJs module',
     author='Slapec',
     url='https://github.com/slapec/django-yaat',
     license='MIT',
-    zip_safe=True,
+    zip_safe=False,
 )
